4d32df9a4736981596533be5507e877ff229bde8
youngah kim <duddk9804@naver.com>
2025-01-07 21:27:45 +0900

    0927
    미디어쿼리 수정

--- js/header.js
+++ js/header.js
@@ -1,19 +1,26 @@
 function isMediaQueryMatch() {
-  return window.innerWidth >= 480 && window.innerWidth <= 720;
+  return window.innerWidth <= 720;
 }
 
 $(document).ready(function () {
-  // 메뉴 열기 버튼 클릭 이벤트
-  $(".header__three-col").click(function () {
+  // 햄버거 버튼 클릭 이벤트
+  $(".header__hamburger").click(function (e) {
     if (isMediaQueryMatch()) {
-      $(".header__nav").addClass("active");
+      $(".header__hamburger, .header__nav").toggleClass("active"); // 메뉴 열기/닫기
+      e.stopPropagation(); // body 클릭 이벤트 방지
     }
   });
 
-  // 메뉴 닫기 버튼 클릭 이벤트
-  $(".header__close-btn").click(function () {
+
+  // 바디 클릭 이벤트
+  $(document).click(function () {
     if (isMediaQueryMatch()) {
-      $(".header__nav").removeClass("active");
+      $(".header__hamburger, .header__nav").removeClass("active"); // 메뉴 닫기
     }
+  });
+
+  // 메뉴 내부 클릭 시 이벤트 전파 차단
+  $(".header__nav").click(function (e) {
+    e.stopPropagation();
   });
 });
